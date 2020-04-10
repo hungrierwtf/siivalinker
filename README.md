16ea42aff2ef96ebbdfbd3be010ad327185db564
hungrier <hungrierwtf@gmail.com>
2020-04-10 13:42:30 -0400

    Committed to GitHub

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # siivalinker
 Link SiIvaGunner videos to their corresponding SiIvaGunner wiki article
+
+# How to use
+Import or paste the .user.js file into your favourite Monkey extension. Whenever you see a [SiIvaGunner video](https://www.youtube.com/watch?v=VlmDPC6ai4Y) on Youtube, it'll put a link next to the title, and you can middle-click to open it in a new tab at your leisure.
+
+# Notes
+If your OS or browser is too old the link may appear to be a bunch of squares. Should still be fine though.
---
+++ siivalinker.user.js
@@ -0,0 +1,149 @@
+// ==UserScript==
+// @name         Siiva linker
+// @namespace    https://hungrierwtf.github.io/
+// @version      0.6
+// @description  Adds a SiIvaGunner wiki link to SiIvaGunner videos on Youtube
+// @author       hungrier
+// @match        http://www.youtube.com/*
+// @match        https://www.youtube.com/*
+// @connect      siivagunner.fandom.com
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+"use strict";
+
+(function() {
+  
+  const dbg = false? function (...args) { console.log.apply(this, args); }: function() {};
+  
+  console.log('setting up');
+  
+  const status = {
+    title: undefined,
+    channel: undefined,
+    siiva: false
+  };
+  
+  const channels = ['SiIvaGunner'];
+  
+  const suggestionUrl = new URL('https://siivagunner.fandom.com/wikia.php');
+  suggestionUrl.searchParams.set('controller', 'LinkSuggest');
+  suggestionUrl.searchParams.set('method', 'getLinkSuggestions');
+  suggestionUrl.searchParams.set('format', 'json');
+  
+  const wikiUrlBase = 'https://siivagunner.fandom.com/wiki/$1';
+  const encSlash = encodeURIComponent('/');
+  
+  let linkel = undefined;
+  
+  const getTextContent = function(q) {
+    let titleH = document.querySelector(q);
+    if (!titleH) {
+      dbg('no element', q);
+      return undefined;
+    } else if (!titleH.textContent) {
+      dbg('no textContent', titleH);
+    }
+
+    return titleH.textContent;
+  }
+  
+  const updateStatus = function(which, selecta, watdo) {
+    let newText = getTextContent(selecta);
+    dbg(which, newText);
+    
+    if (newText !== status[which]){
+      dbg('updating', which, status[which], '->', newText);
+      status[which] = newText;
+      
+      if (watdo) {
+        dbg('doing wat')
+        watdo();
+      }
+    }
+  }
+  
+  const putLinkel = function() {
+    linkel = document.createElement('a');
+    linkel.text = '🤧🧛‍♀️🔫🤓';
+    linkel.style.color = '#47e';
+    linkel.style.marginLeft = '1em';
+
+    document.querySelector('#info-contents .title').appendChild(linkel);
+    dbg('linkel linked');
+  }
+  
+  const checkSiiva = function() {
+    let isSiiva = channels.indexOf(status.channel) > -1;
+    dbg('updating siivs status', isSiiva, status.channel);
+    status.siiva = isSiiva;
+    
+    if (!linkel) {
+      putLinkel();
+    }
+    
+    linkel.style.visibility = isSiiva? 'visible': 'hidden';
+  }
+  
+  const updateLink = function(href) {
+    if (!linkel) {
+      putLinkel();
+    }
+    
+    linkel.href = href;
+    console.log('link updated', href);
+  }
+  
+  const processSuggestions = function(res) {
+    if (res.status === 200) {
+      dbg('response:', res.responseText);
+      
+      let resObj = JSON.parse(res.responseText);
+      dbg('resojb', resObj);
+      
+      let suggestions = resObj.suggestions;
+      if (suggestions.length > 0) {
+        let valueEnc = encodeURIComponent(suggestions[0].replace(/ /g, '_'));
+        let wikiUrl = wikiUrlBase.replace(/\$1/, valueEnc).replace(encSlash, '/');
+        dbg('wiki url', wikiUrl);
+        
+        updateLink(wikiUrl);
+      } else {
+        dbg('no suggestion', resObj);
+        updateLink();
+      }
+    } else {
+      console.log('something bad hapepned', res);
+    }
+  }
+  
+  const startGetSuggestion = function(query) {
+    suggestionUrl.searchParams.set('query', query);
+    dbg('query', query);
+
+    let xhrOptions = {
+      method: 'GET',
+      url: suggestionUrl,
+      onload: processSuggestions
+    }
+    GM_xmlhttpRequest(xhrOptions);
+  }
+  
+  const getLink = function() {
+    if (status.siiva) {
+      startGetSuggestion(status.title);
+    }
+  }
+
+  const loop = function() {
+    updateStatus('channel', '#channel-name #text', checkSiiva);
+    
+    updateStatus('title', '#info-contents .title yt-formatted-string', getLink);
+    
+    setTimeout(loop, 1000);
+  }
+
+  console.log('starting');
+  
+  setTimeout(loop);
+})();
