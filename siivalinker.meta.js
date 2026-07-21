ef3d222abcc27bca0826fc037a6eef4008b66755
hungrierwtf <hungrierwtf@gmail.com>
2026-07-21 01:23:44 -0400

    Updated to use new Siivagunner wiki

---
+++ siivalinker.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Siiva linker
+// @namespace    https://hungrierwtf.github.io/
+// @version      1.0.0
+// @description  Adds a SiIvaGunner wiki link to SiIvaGunner videos on Youtube
+// @author       hungrier
+// @match        http://www.youtube.com/*
+// @match        https://www.youtube.com/*
+// @connect      siivagunner.fandom.com
+// @connect      siivagunner.wiki
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
--- siivalinker.user.js
+++ siivalinker.user.js
@@ -1,75 +1,75 @@
 // ==UserScript==
 // @name         Siiva linker
 // @namespace    https://hungrierwtf.github.io/
-// @version      0.9.2
+// @version      1.0.0
 // @description  Adds a SiIvaGunner wiki link to SiIvaGunner videos on Youtube
 // @author       hungrier
 // @match        http://www.youtube.com/*
 // @match        https://www.youtube.com/*
 // @connect      siivagunner.fandom.com
+// @connect      siivagunner.wiki
 // @grant        GM_xmlhttpRequest
 // @homepageURL  https://github.com/hungrierwtf/siivalinker
+// @updateURL    https://github.com/hungrierwtf/siivalinker/raw/master/siivalinker.meta.js
 // @downloadURL  https://github.com/hungrierwtf/siivalinker/raw/master/siivalinker.user.js
 // ==/UserScript==
 
 "use strict";
 
 (function() {
-  
+
   const dbg = false? function (...args) { console.log.apply(this, args); }: function() {};
-  
+
   console.log('setting up');
-  
+
   const status = {
     title: undefined,
     channel: undefined,
     siiva: false
   };
-  
+
   const channels = ['SiIvaGunner', 'Flustered Fernando'];
-  
-  const suggestionUrl = new URL('https://siivagunner.fandom.com/wikia.php');
-  suggestionUrl.searchParams.set('controller', 'LinkSuggest');
-  suggestionUrl.searchParams.set('method', 'getLinkSuggestions');
-  suggestionUrl.searchParams.set('format', 'json');
-  
-  const wikiUrlBase = 'https://siivagunner.fandom.com/wiki/$1';
+
+  const suggestionUrl = new URL('https://www.siivagunner.wiki/w/rest.php/v1/search/title');
+    suggestionUrl.searchParams.set('limit', '10');
+
+  const wikiUrlBase = 'https://www.siivagunner.wiki/wiki/$1';
   const encSlash = encodeURIComponent('/');
-  
+
   const thinger = (function() {
     const meColor = '#47e';
-    
+
     const container = document.createElement('span');
     container.style.color = meColor;
     container.style.marginLeft = '1em';
     container.style.display = 'inline-block';
-    
+
     let attached = false;
-    
+
     const attachMe = function() {
       if (attached) {
         return;
       }
-      
+
       const titleEl = document.querySelector('div#above-the-fold div#title.ytd-watch-metadata h1');
       if (!titleEl) {
         console.log('no title el to attach');
         return;
       }
-      
+
       titleEl.appendChild(container);
       console.log('attached to title el');
       attached = true;
     }
-    
+
     const showEl = function(el, show) {
-      if (show) { 
-        attachMe(); 
+      if (show) {
+        attachMe();
       }
-      
+
       el.style.display = show? 'inline': 'none';
     }
-    
+
     const link = document.createElement('a');
     link.text = '🤧🧛‍♀️🔫🤓';
     link.style.textDecoration = 'none';
@@ -77,11 +77,11 @@
     link.style.borderWidth = '0 4px 1px 0';
     link.style.borderStyle = 'solid';
     container.appendChild(link);
-    
+
     const lunk = document.createElement('span');
     lunk.appendChild(document.createTextNode('🧻'));
     container.appendChild(lunk);
-    
+
     const setLink = function(url) {
       if (url) {
         link.href = url;
@@ -91,15 +91,15 @@
         showEl(link, false);
         showEl(lunk, true);
       }
-      
+
       console.log('updated link', url);
     }
-    
+
     const hideBoth = function() {
       showEl(link, false);
       showEl(lunk, false);
     }
-    
+
     const me = {
       contEl: container,
       linkEl: link,
@@ -107,10 +107,10 @@
       setLink: setLink,
       hide: hideBoth
     };
-    
+
     return me;
   })();
-  
+
   const getTextContent = function(q) {
     let titleH = document.querySelector(q);
     if (!titleH) {
@@ -122,45 +122,49 @@
 
     return titleH.textContent;
   }
-  
+
   const updateStatus = function(which, selecta, watdo) {
     let newText = getTextContent(selecta);
     dbg(which, newText);
-    
+
     if (newText !== status[which]){
       dbg('updating', which, status[which], '->', newText);
       status[which] = newText;
-      
+
       if (watdo) {
         dbg('doing wat')
         watdo();
       }
     }
   }
-  
+
   const checkSiiva = function() {
     let isSiiva = channels.indexOf(status.channel) > -1;
     dbg('updating siivs status', isSiiva, status.channel);
     status.siiva = isSiiva;
-    
+
     if (!isSiiva) {
       thinger.hide();
     }
   }
-  
+
   const processSuggestions = function(res) {
     if (res.status === 200) {
       dbg('response:', res.responseText);
-      
+
       let resObj = JSON.parse(res.responseText);
       dbg('resojb', resObj);
-      
-      let suggestions = resObj.suggestions;
+
+      let suggestions = resObj.pages;
+      dbg('suggestions', suggestions);
+
       if (suggestions.length > 0) {
-        let valueEnc = encodeURIComponent(suggestions[0].replace(/ /g, '_'));
+        let sug = suggestions[0];
+        dbg('sug', sug);
+        let valueEnc = sug.key;
         let wikiUrl = wikiUrlBase.replace(/\$1/, valueEnc).replace(encSlash, '/');
         dbg('wiki url', wikiUrl);
-        
+
         thinger.setLink(wikiUrl);
       } else {
         dbg('no suggestion', resObj);
@@ -170,9 +174,9 @@
       console.log('something bad hapepned', res);
     }
   }
-  
+
   const startGetSuggestion = function(query) {
-    suggestionUrl.searchParams.set('query', query);
+    suggestionUrl.searchParams.set('q', query);
     dbg('query', query);
 
     let xhrOptions = {
@@ -182,21 +186,21 @@
     }
     GM_xmlhttpRequest(xhrOptions);
   }
-  
+
   const processTitle = function(title) {
     if (!title) {
       return title;
     }
-    
+
     title = title.trim();
     dbg('trimmed', title);
-    
+
     title = title.replace('#', '');
     dbg('de-numberized', title);
-    
+
     return title;
   }
-  
+
   const getLink = function() {
     if (status.siiva) {
       const query = processTitle(status.title);
@@ -207,13 +211,13 @@
 
   const loop = function() {
     updateStatus('channel', '#channel-name #text', checkSiiva);
-    
+
     updateStatus('title', '#info-contents .title yt-formatted-string', getLink);
-    
+
     setTimeout(loop, 1000);
   }
 
   console.log('starting');
-  
+
   setTimeout(loop);
 })();
